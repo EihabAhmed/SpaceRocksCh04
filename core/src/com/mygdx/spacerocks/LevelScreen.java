b0913689ee2c690a6e4849f276106d4b845cb651
EihabAhmed <eihabahmed@gmail.com>
2023-05-14 00:29:46 +0300

    Added game sprites

--- core/src/com/mygdx/spacerocks/LevelScreen.java
+++ core/src/com/mygdx/spacerocks/LevelScreen.java
@@ -1,5 +1,7 @@
 package com.mygdx.spacerocks;
 
+import com.badlogic.gdx.Input.Keys;
+
 public class LevelScreen extends BaseScreen {
     private Spaceship spaceship;
 
@@ -10,9 +12,56 @@ public class LevelScreen extends BaseScreen {
         BaseActor.setWorldBounds(space);
 
         spaceship = new Spaceship(400, 300, mainStage);
+
+        new Rock(600, 500, mainStage);
+        new Rock(600, 300, mainStage);
+        new Rock(600, 100, mainStage);
+        new Rock(400, 100, mainStage);
+        new Rock(200, 100, mainStage);
+        new Rock(200, 300, mainStage);
+        new Rock(200, 500, mainStage);
+        new Rock(400, 500, mainStage);
     }
 
     public void update(float dt) {
+        for (BaseActor rockActor : BaseActor.getList(mainStage, "com.mygdx.spacerocks.Rock")) {
+            if (rockActor.overlaps(spaceship)) {
+                if (spaceship.shieldPower <= 0) {
+                    Explosion boom = new Explosion(0, 0, mainStage);
+                    boom.centerAtActor(spaceship);
+                    spaceship.remove();
+                    spaceship.setPosition(-1000, -1000);
+                } else {
+                    spaceship.shieldPower -= 34;
+                    Explosion boom = new Explosion(0, 0, mainStage);
+                    boom.centerAtActor(rockActor);
+                    rockActor.remove();
+                }
+            }
 
+            for ( BaseActor laserActor : BaseActor.getList(mainStage, "com.mygdx.spacerocks.Laser") )
+            {
+                if (laserActor.overlaps(rockActor))
+                {
+                    Explosion boom = new Explosion(0,0, mainStage);
+                    boom.centerAtActor(rockActor);
+                    laserActor.remove();
+                    rockActor.remove();
+                }
+            }
+        }
+    }
+
+    // override default InputProcessor method
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Keys.X)
+            spaceship.warp();
+
+        if (keycode == Keys.SPACE)
+            spaceship.shoot();
+
+        return false;
     }
 }
--- core/src/com/mygdx/spacerocks/Spaceship.java
+++ core/src/com/mygdx/spacerocks/Spaceship.java
@@ -2,11 +2,15 @@ package com.mygdx.spacerocks;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Spaceship extends BaseActor {
 
     private Thrusters thrusters;
+
+    private Shield shield;
+    public int shieldPower;
 
     public Spaceship(float x, float y, Stage s) {
         super(x, y, s);
@@ -21,6 +25,11 @@ public class Spaceship extends BaseActor {
         thrusters = new Thrusters(0, 0, s);
         addActor(thrusters);
         thrusters.setPosition(-thrusters.getWidth(), getHeight() / 2 - thrusters.getHeight() / 2);
+
+        shield = new Shield(0, 0, s);
+        addActor(shield);
+        shield.centerAtPosition(getWidth() / 2, getHeight() / 2);
+        shieldPower = 100;
     }
 
     public void act(float dt) {
@@ -42,5 +51,30 @@ public class Spaceship extends BaseActor {
         applyPhysics(dt);
 
         wrapAroundWorld();
+
+        shield.setOpacity(shieldPower / 100f);
+        if (shieldPower <= 0)
+            shield.setVisible(false);
+    }
+
+    public void warp() {
+        if (getStage() == null)
+            return;
+
+        Warp warp1 = new Warp(0, 0, this.getStage());
+        warp1.centerAtActor(this);
+        setPosition(MathUtils.random(800), MathUtils.random(600));
+        Warp warp2 = new Warp(0, 0, this.getStage());
+        warp2.centerAtActor(this);
+    }
+
+    public void shoot() {
+        if (getStage() == null)
+            return;
+
+        Laser laser = new Laser(0, 0, this.getStage());
+        laser.centerAtActor(this);
+        laser.setRotation(this.getRotation());
+        laser.setMotionAngle(this.getRotation());
     }
 }
